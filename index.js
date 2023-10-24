40a6bf9abc9c5ada78a9da88aa905bafba7df3bd
MehdiFarshbaf <mfarshbaf92@yahoo.com>
2023-10-24 23:50:44 +0330

    start project

---
+++ index.js
@@ -0,0 +1,33 @@
+import express from 'express'
+import dotenv from 'dotenv'
+
+import {connectDB} from "./config/db.js";
+import {headers} from "./middlewares/headers.js";
+import {errorHandler} from "./middlewares/error.js";
+
+// routers import
+
+// Load Config
+dotenv.config()
+connectDB()
+
+const app = new express()
+
+// BodyParser & Headers
+app.use(express.json())
+app.use(headers)
+
+// File Upload Middleware
+
+// Static Folder
+
+// Routes
+
+// error handler
+app.use(errorHandler)
+
+const PORT = process.env.PORT || 3000
+
+app.listen(PORT, () => {
+    console.log(`Server running in ${process.env.NODE_ENV} mode on port ${PORT}`)
+})
